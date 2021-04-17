3577ed1104c81ae4453219ecca28776954721dc9
alxayeed <alxayeed@gmail.com>
2021-04-17 12:52:34 +0600

    single quote and author is scrapped and displayed in template

--- web scrapper/web_scrapper.py
+++ web scrapper/web_scrapper.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+
 app = Flask(__name__)
+
+source = requests.get('http://quotes.toscrape.com/').text
+soup = BeautifulSoup(source, 'lxml')
+# get quote text
+quote = soup.find('div', class_='quote')
+quote_text = quote.span.text
+# get author
+author = soup.find('small', class_='author').text
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', quote=quote_text, author=author)
 
 
 if __name__ == '__main__':
